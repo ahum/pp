8c5b7656dcf77c684d2eec0eafaee930d18395b3
ed eustace <ed.eustace@gmail.com>
2021-11-08 21:26:15 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -13,27 +13,44 @@ struct Opt {
     message: String,
     #[structopt(short = "l", long = "level", default_value = "level")]
     level: String,
+    #[structopt(short = "fl", long = "filter_level", default_value = "silly")]
+    filter_level: String,
     // #[structopt(short = "f", long = "filter", default_value = vec![])]
     // filter: Vec<String>,
 }
 
+const LEVELS: [&str; 7] = ["silly", "debug", "verbose", "info", "warn", "error", "off"];
+
+fn allow_level(requested: &str, limit: &str) -> bool {
+    let requested_index = LEVELS.iter().position(|&x| x == requested);
+    let limit_index = LEVELS.iter().position(|&x| x == limit);
+
+    println!("requested {}, limit: {}", requested, limit);
+    match (requested_index, limit_index) {
+        (Some(r), Some(li)) => r >= li,
+        _ => false,
+    }
+}
+
 fn main() {
     let opt = Opt::from_args();
-    println!("{:?}", opt);
-    if atty::is(Stream::Stdin) {
-        //println!("I'm not");
-    } else {
+    let opt_level = opt.level.as_str();
+    // println!("{:?}", opt);
+    if !atty::is(Stream::Stdin) {
         let stdin = io::stdin();
         let lines: Vec<String> = stdin.lock().lines().flatten().collect();
         let together = lines.join("\n");
 
         match serde_json::from_str::<Value>(&together) {
             Result::Ok(v) => {
-                println!(
-                    "[{}] {}",
-                    v[opt.level].as_str().unwrap().blue(),
-                    v[opt.message].as_str().unwrap_or("")
-                );
+                let level = v[opt_level].as_str().unwrap();
+                if allow_level(level, opt_level) {
+                    println!(
+                        "[{}] {}",
+                        level.blue(),
+                        v[opt.message].as_str().unwrap_or("")
+                    );
+                }
             }
             _ => {
                 // do nothing
